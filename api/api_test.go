9b11254cc1aa9a95dc9cc7ef3c6a8f3c48b80618
Alessio Martorelli <alessio.martorelli@newvoicemedia.com>
2018-10-09 14:14:30 +0100

    Adding some comments to api tests

---
+++ api/api_test.go
@@ -0,0 +1,7 @@
+package api_test
+
+import "testing"
+
+func TestPurgeHandler(t *testing.T) {
+
+}
--- api/conf_test.go
+++ api/conf_test.go
@@ -5,22 +5,24 @@ import (
 	"particles/api"
 	"testing"
 
-	"github.com/sirupsen/logrus"
 	yaml "gopkg.in/yaml.v2"
 )
 
+// validConf is a perfectly valid API configuration
 var validConf = `address: 192.168.1.10
 port: 1234
 cert_file: /tmp/test.crt
 key_file: /tmp/test.key
 `
 
+// invalidAddress is an invalid configuration because the address isn't a valid IP
 var invalidAddress = `address: 192.168.1
 port: 1234
 cert_file: /tmp/test.crt
 key_file: /tmp/test.key
 `
 
+// invalidPort is an invalid configuration because the port is a negative number
 var invalidPort = `address: 192.168.1.10
 port: -1
 cert_file: /tmp/test.crt
@@ -42,7 +44,7 @@ func TestIsValid(t *testing.T) {
 		c := api.Conf{}
 		err := yaml.Unmarshal([]byte(test.in), &c)
 		if err != nil {
-			logrus.Fatalf("invalid config: %s", err)
+			t.Errorf("invalid config: %s", err)
 		}
 
 		valid, _ := c.IsValid()
