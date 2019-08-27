bd72a1ac883795047709ece92ac661a03bcdf788
Alessio Martorelli <alessio.martorelli@gmail.com>
2019-08-27 17:04:52 +0100

    fixed api tests

--- pkg/cache/metrics.go
+++ pkg/cache/metrics.go
@@ -9,30 +9,30 @@ var (
 	lookupMetric = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "particles_cache_lookup_total",
 		Help: "Cache lookup count",
-	}, []string{"domain", "type", "status"})
+	}, []string{"type", "status"})
 
 	lookupDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name: "particles_cache_lookup_seconds",
 		Help: "Cache lookup duration",
-	}, []string{"domain", "type"})
+	}, []string{"type"})
 
 	storeMetric = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "particles_cache_store_total",
 		Help: "Store count",
-	}, []string{"domain", "type", "status"})
+	}, []string{"type", "status"})
 
 	storeDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name: "particles_cache_store_seconds",
 		Help: "Cache store duration",
-	}, []string{"domain", "type"})
+	}, []string{"type"})
 
 	purgeMetric = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "particles_cache_purge_total",
 		Help: "Purge count",
-	}, []string{"domain", "type", "status"})
+	}, []string{"type", "status"})
 
 	purgeDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name: "particles_cache_purge_seconds",
 		Help: "Cache purge duration",
-	}, []string{"domain", "type"})
+	}, []string{"type"})
 )
